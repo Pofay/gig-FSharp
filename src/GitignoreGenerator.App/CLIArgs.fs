bf15e2c0629fa5265d6e9524417767b92b1c2e9a
Pofay <gianCarlo.gilos@outlook.com>
2019-04-04 12:50:33 +0800

    Made possible prototype

--- src/GitignoreGenerator.App/CLIArgs.fs
+++ src/GitignoreGenerator.App/CLIArgs.fs
@@ -5,13 +5,13 @@ open Argu
 module CLIArgs = 
 
   type CLIArguments =
-    | [<Mandatory>] Contents of tags:string
+    | Ignore_Tags of tags:string
     | Directory of path:string
     | Mode of mode:string
   with
      interface IArgParserTemplate with
        member s.Usage =
           match s with
-            | Contents _ -> "This is a mandatory option since the tags are needed for gitignore content, Example: linux,java || CSharp,Windows"
+            | Ignore_Tags _ -> "This is a mandatory option since the tags are needed for gitignore content, Example: linux,java || CSharp,Windows"
             | Directory _ -> "The directory you wish to create the .gitignore file"
             | Mode _ -> "Example: append || overwrite"
--- src/GitignoreGenerator.App/GitignoreGenerator.App.fs
+++ src/GitignoreGenerator.App/GitignoreGenerator.App.fs
@@ -2,12 +2,59 @@ module GitignoreGenerator.App
 
 open Argu
 open GitignoreGenerator.Logic
+open FSharpPlus
+open FSharp.Data
+open System.IO
+open System
+
+
+let fetchGitIgnoreOrDoNothing api (ignoreTagsOrNone: string option) = async {
+    return! 
+     match ignoreTagsOrNone with
+        | Some v -> Http.AsyncRequestString(api + "/" + v)
+        | None _ -> async {  let empty = "" 
+                             return empty }
+}
+
+let asyncWriteFile fileName contents = async {
+    File.WriteAllText(fileName, contents)
+}
+
+let asyncAppendFile fileName contents = async {
+    File.AppendAllText(fileName, contents)
+}
+
+let asyncDoNothing fileName contents = async { ignore() }
+
+let determineMode fileName mode = 
+   match mode with
+   | "append" -> asyncAppendFile fileName
+   | "overwrite" -> asyncWriteFile fileName
+   | _ -> asyncDoNothing fileName
+
+
+let unsafeGetCurrentDirectory = Environment.CurrentDirectory + "/.gitignore"
 
 
 [<EntryPoint>]
 let main argv =
-
     let parser = ArgumentParser.Create<CLIArgs.CLIArguments>(programName = "gig.exe")
-    let usage = parser.PrintUsage()
-    printfn "%s" usage
-    0 // return an integer exit code
+    let api = "https://www.gitignore.io/api/"
+
+    let results = parser.Parse(argv)
+
+    let ignoreTagsOrNone = results.TryGetResult CLIArgs.CLIArguments.Ignore_Tags
+    let directory = results.TryGetResult CLIArgs.CLIArguments.Directory 
+                  |> map(fun dir -> dir + "/.gitignore") 
+                  |> Option.defaultValue unsafeGetCurrentDirectory
+
+    let operation = results.TryGetResult CLIArgs.CLIArguments.Mode 
+                 |> Option.defaultValue "overwrite"
+                 |> determineMode directory
+    
+    
+    ignoreTagsOrNone |> (fetchGitIgnoreOrDoNothing api >=> operation) |> Async.RunSynchronously
+
+    0
+
+
